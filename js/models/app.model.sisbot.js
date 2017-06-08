7be9af949d9140815e04b4c8cf56e980a6fe9523
kieftrav <kiefertravis@gmail.com>
2017-06-08 10:05:00 -0500

    Bug fix.

--- js/models/app.model.sisbot.js
+++ js/models/app.model.sisbot.js
@@ -163,6 +163,8 @@ app.model.sisbot = {
 				this.set('installing_updates', 'false');
 				location.reload();
 			}
+			
+			self.set('reason_unavailable', '');
 		}
 	},
 	/**************************** SISBOT ADMIN ********************************/
