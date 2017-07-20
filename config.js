00f3e574e0b4599ca3553716f333f933b477a545
kieftrav <kiefertravis@gmail.com>
2017-07-20 07:06:21 -0500

    Hide IP on web.

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 var config = {
-    version : '0.5.14',
+    version : '0.5.15',
     debug   : true,
     env     : process.env.NODE_ENV,
     travis_dev: {
--- js/gen/app.config.js
+++ js/gen/app.config.js
@@ -1,6 +1,6 @@
 app.config = {
 	env					: 'beta',
-	version				: '0.5.14',
+	version				: '0.5.15',
 	envs	: {
 		alpha: {	// loads local data only
 			base_url	: 'http://app.dev.withease.io:3000/',
