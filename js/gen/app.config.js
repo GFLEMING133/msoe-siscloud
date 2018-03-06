121fa5066b701b9e3d33a92319180c6a130a2f54
kieftrav <kiefertravis@gmail.com>
2018-03-06 10:54:09 -0600

    Bump build to 48. Fix isues with displaying icons for Apple's internal review mode.

--- js/gen/app.config.js
+++ js/gen/app.config.js
@@ -1,6 +1,6 @@
 app.config = {
-	env					: 'beta',
-	version				: '1.2.46',
+	env					: 'alpha',
+	version				: '1.2.48',
 	envs	: {
 		alpha: {	// loads local data only
 			base_url	: 'http://app.dev.withease.io:3001/',
