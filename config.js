e05d6c188dfb485f5ce73d469f3fab84b4b0e3bb
CurtisMorice <curtismorice@gmail.com>
2019-02-04 15:06:05 -0600

    Gettign ready to add to tTest Flight and GOogle Play, Tested with Micah and unale to resolve scrolling on Ios.

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 var config = {
-    version : '1.7', // Fixed scrolling issues in Edit Playlists
+    version : '1.7.1', // Ready to push to TestFlight and Google Play , Fixed scrolling issues in Edit Playlists
     debug   : true,
     env     : process.env.NODE_ENV,
     travis_dev: {
