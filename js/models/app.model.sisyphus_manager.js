9dbd18ab5a58486404664690ffac13ffe91dd900
CurtisMorice <curtismorice@gmail.com>
2019-02-07 15:59:08 -0600

    Moved app trigger for routing to test for Ios

--- js/models/app.model.sisyphus_manager.js
+++ js/models/app.model.sisyphus_manager.js
@@ -980,7 +980,7 @@ app.model.sisyphus_manager = {
 
 			app.current_session().add_nx('sisbot_hostnames', sisbot_hostname);
 			app.current_session().save_session();
-			app.trigger('session:active', { secondary: 'false', primary: 'current' });
+			
 			// hotspot access allows not requiring user
 			if (self.get_model('user_id')) {
 				self.get_model('user_id').add_nx('data.sisbot_ids', self.get('sisbot_id'));
@@ -988,7 +988,7 @@ app.model.sisyphus_manager = {
 				self.get_model('user_id').save(true);
 			}
 		}, 0);
-			
+		app.trigger('session:active', { secondary: 'false', primary: 'current' });	
 	},
 	/**************************** NETWORK INFO **********************************/
 	get_network_ip_address: function (cb) {
