57576573c92fca9ad00a8abad52d9d7e56ec7377
CurtisMorice <curtismorice@gmail.com>
2019-07-02 08:25:57 -0500

    Getting readyt to push Up

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 var config = {
-    version : '1.8.13', //Updated cordova iOS to 5.0.1, cordova Android to 8.0.0, updated WifiWizard / polygonproducts to WiFiWizard2 and changed code in siscloud. Updated other plugins and removed those that were deprocated. Changed android:usesCleartextTraffic=true in the AndroidManifest to correct launch error of not connecting to wiFi per Matt K , Updated gradle to version 5.4.1 . ALl is tested and working for the API level that has been upgraded to 28 per Google Plays notice of necessary upgrade. 
+    version : '1.8.14', //Pushing Webcenter for company testing.
     debug   : true,
     env     : process.env.NODE_ENV,
     matt_dev: {
