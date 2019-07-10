a36ae492d36a3b2f4095fd1b8b441f162cbd5e5f
CurtisMorice <curtismorice@gmail.com>
2019-07-10 13:39:55 -0500

    //staging branch push with Sis Firmware Version 1.10.29 for update changes to regression test. Also removed pull-top css attribute from cumminty page to remove white bar

--- js/gen/app.config.js
+++ js/gen/app.config.js
@@ -1,6 +1,6 @@
 app.config = {
 	env					: 'prod',
-	version				: '1.8.10', //pushing to test flight
+	version				: '1.8.20', //staging branch push with Sis Firmware Version 1.10.29 for update changes to regression test.
 	envs	: {
 		alpha: {	// loads local data only
 			base_url	: 'http://app.dev.withease.io:3001/', // local
