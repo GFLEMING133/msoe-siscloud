271a008d30d85cc6827774c6f6a0026451cc0f70
CurtisMorice <curtismorice@gmail.com>
2019-07-30 16:15:53 -0500

    Added 1.8.28 to CHANGELOG.md and fixed urls to webcenter in the app.config

--- js/gen/app.config.js
+++ js/gen/app.config.js
@@ -12,15 +12,15 @@ app.config = {
 		},
 		beta: {		// tests local network
 			base_url	: 'http://app.dev.withease.io:3001/', //local url
-			api_url		: 'http://dev.webcenter.sisyphus-industries.com/', // add entry in your computers /etc/hosts mapped to your bot's IP address
-			web_url		: 'http://dev.webcenter.sisyphus-industries.com/', //web_center url	***Change to this for Rails web_center= http://localhost:3000/  (aka rails s) //  10.0.0.3	beta_bot.local
+			api_url		: 'https://webcenter.sisyphus-industries.com/', // add entry in your computers /etc/hosts mapped to your bot's IP address
+			web_url		: 'https://webcenter.sisyphus-industries.com/', //web_center url	***Change to this for Rails web_center= http://localhost:3000/  (aka rails s) //  10.0.0.3	beta_bot.local
 			sisbot_url  : 'http://192.168.86.20', //talking to sisbot    //  ... or just put your URL in here '192.168.XX.XXX' << for local Dev Env --insert your ip address + 3000
 			port		: 3001, //work=192.168.1.168:3002 home=192.168.1.5:3002
 		},
 		sisbot: {
 			base_url	: window.location.href,
-			api_url		: 'http://webcenter.sisyphus-industries.com/',
-			web_url		: 'http://webcenter.sisyphus-industries.com/',
+			api_url		: 'https://webcenter.sisyphus-industries.com/',
+			web_url		: 'https://webcenter.sisyphus-industries.com/',
 			sisbot_url  : window.location.href, //talking to sisbot
 			port		: 3001,
 		},
