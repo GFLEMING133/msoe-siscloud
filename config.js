aa1fb3eff967b870c32e657851a1cf300caac292
CurtisMorice <curtismorice@gmail.com>
2019-09-24 23:59:38 -0500

    added input fo rhex in primary and secondary html, removed padding on color picker for scroll puposes on smaller phones added border left to slider and removed color picker background color

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 var config = {
-  version				: '1.8.58', // Color Picker bugfixes
-    debug     : true,
-    env       : process.env.NODE_ENV,
+	  version				: '1.8.57', // Color Picker bugfixes, added hex input field, removed color picker padding for scroll issues on smaller phones
+    debug         : true,
+    env           : process.env.NODE_ENV,
     matt_dev: {
-      base_dir	: '/Users/mattfox12/Documents/Sodo/Ease/app',
-      port			: 3001,
-      base_url  : 'http://app.dev.withease.io:3000/'
+      base_dir	  : '/Users/mattfox12/Documents/Sodo/Ease/app',
+      port			  : 3001,
+      base_url    : 'http://app.dev.withease.io:3000/'
     }
 };
 
--- js/gen/app.config.js
+++ js/gen/app.config.js
@@ -1,6 +1,6 @@
 app.config = {
 	env					: 'prod',
-	version				: '1.8.58', // Color Picker bugfixes
+	version				: '1.8.57', // Color Picker bugfixes, added hex input field, removed color picker padding for scroll issues on smaller phones
 	disconnect_timeout_to_stop_polling: 45000, // stop trying to find tables after 45 seconds
 	envs	: {
 		alpha: {	// loads local data only **5 tap on No Table Found Screen to launch shell app
@@ -12,7 +12,7 @@ app.config = {
 		},
 		beta: {		// tests local network
 			base_url	: 'http://app.dev.withease.io:3001/', //local url
-			api_url		: 'https://webcenter.sisyphus-industries.com/', // add entry in your computers /etc/hosts mapped to your bot's IP address
+			api_url		: 'http://localhost:3000/', // add entry in your computers /etc/hosts mapped to your bot's IP address
 			web_url		: 'http://localhost:3000/', //web_center url	***Change to this for Rails web_center= http://localhost:3000/  (aka rails s) //  10.0.0.3	beta_bot.local
 			sisbot_url  : 'http://192.168.1.5:3002', //talking to sisbot    //  ... or just put your URL in here '192.168.XX.XXX:3002' << for local Dev Env --insert your ip address + 3000
 			port		: 3001,
