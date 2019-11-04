07f79f72fe5c48dbd928495e8c1ed59a0e4d648e
CurtisMorice <curtismorice@gmail.com>
2019-11-04 14:26:11 -0600

    added place for MAC Address but cannot find it, commit to try and pull

--- js/app.config.js
+++ js/app.config.js
@@ -19,7 +19,7 @@ app.config = {
 			web_url		: 'https://webcenter.sisyphus-industries.com/',
 			// api_url		: 'http://localhost:3333/',// add entry in your computers /etc/hosts mapped to your bot's IP address
 			// web_url		: 'http://localhost:3333/', //web_center url	***Change to this for Rails web_center= http://localhost:3333/  (aka rails s) //  10.0.0.3	beta_bot.local
-			sisbot_url  : 'http://192.168.1.140:3002', //talking to sisbot    //  ... or just put your URL in here '192.168.XX.XXX:3002' << for local Dev Env --insert your ip address + 3000
+			sisbot_url  : 'http://192.168.1.5:3002', //talking to sisbot    //  ... or just put your URL in here '192.168.XX.XXX:3002' << for local Dev Env --insert your ip address + 3000
 			port		: 3001,
 			show_tg		: true
 		},
@@ -27,7 +27,7 @@ app.config = {
 			base_url	: 'http://app.dev.withease.io:3001/', //local url
 			api_url		: 'https://webcenter.sisyphus-industries.com/',
 			web_url		: 'https://webcenter.sisyphus-industries.com/',
-			sisbot_url  : 'http://192.168.1.140:3002', //talking to sisbot    //  ... or just put your URL in here '192.168.XX.XXX:3002' << for local Dev Env --insert your ip address + 3000
+			sisbot_url  : 'http://192.168.1.5:3002', //talking to sisbot    //  ... or just put your URL in here '192.168.XX.XXX:3002' << for local Dev Env --insert your ip address + 3000
 			port		: 3001,
 			show_tg		: true
 		},
