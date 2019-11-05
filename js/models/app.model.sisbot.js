2464a45c8a9bdcb431dca13fdf1d8f89f27bb4e2
CurtisMorice <curtismorice@gmail.com>
2019-11-05 09:22:25 -0600

    corrected night mode and added this.set() in the model.sisbot for tables without

--- js/models/app.model.sisbot.js
+++ js/models/app.model.sisbot.js
@@ -934,7 +934,7 @@ app.model.sisbot = {
 		_.extend(data, edit);
 
 		data.timezone_offset = moment().format('Z');
-
+		this.set('data.is_sleep_enabled', edit.is_sleep_enabled);
 		this._update_sisbot('set_sleep_time', data, function(obj) {
 			if (obj.err) {
 				self.set('errors', [ obj.err ]);
