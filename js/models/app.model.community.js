d93cc418ce6e1f11f5a3baedfd4f6a89e278c20d
CurtisMorice <curtismorice@gmail.com>
2020-01-02 16:48:28 -0500

    fixed overlays, added search bars to community pages and flipped hamburger and cloud

--- js/models/app.model.community.js
+++ js/models/app.model.community.js
@@ -24,7 +24,7 @@ app.model.community = {
       limit_step    : 30,
       fetching_more : 'false',
 
-      hamburger : 'false',
+      hamburger     : 'false',
 
       data: {
         id      : data.id,
