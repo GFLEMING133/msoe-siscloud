e3c3d3fe3d2fbb427e8960ec0132f15e6f9fd67a
CurtisMorice <curtismorice@gmail.com>
2020-01-17 10:59:54 -0500

    changed is_simulator=false

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ var app = {
 	view		: {},
 	tour		: {},
 	is_production: false, // disallow all console logs
-	is_simulator: true, // are we in a simulator? Mainly for handling bluetooth
+	is_simulator: false, // are we in a simulator? Mainly for handling bluetooth
 	simulator_ip: '192.168.0.19', // force a specific network address in simulator
 	is_visible: true,
 	mouse_is_down: false,
