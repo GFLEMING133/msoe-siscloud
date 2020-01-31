0ed9435d697ad79e38c09f84df377c58c0bc0f94
CurtisMorice <curtismorice@gmail.com>
2020-01-31 13:49:17 -0500

    fixed scroll box, added conditional for Default Tracks to only not show in header, pointed config to dev, removed limit and offset as it is not necessary.

--- js/app.config.js
+++ js/app.config.js
@@ -14,12 +14,12 @@ app.config = {
 		},
 		beta: {		// tests local network
 			base_url		: 'http://app.dev.withease.io:3001/', //local url
-			// api_url		: 'http://dev.webcenter.sisyphus-industries.com/',
-			// web_url		: 'http://dev.webcenter.sisyphus-industries.com/',
-			// api_url		: 'https://webcenter.sisyphus-industries.com/',
-			// web_url		: 'https://webcenter.sisyphus-industries.com/',
-			api_url			: 'http://localhost:3333/',// add entry in your computers /etc/hosts mapped to your bot's IP address
-			web_url			: 'http://localhost:3333/', //web_center url	***Change to this for Rails web_center= http://localhost:3333/  (aka rails s) //  10.0.0.3	beta_bot.local
+			api_url			: 'http://dev.webcenter.sisyphus-industries.com/',
+			web_url			: 'http://dev.webcenter.sisyphus-industries.com/',
+			// api_url			: 'https://webcenter.sisyphus-industries.com/',
+			// web_url			: 'https://webcenter.sisyphus-industries.com/',
+			// api_url			: 'http://localhost:3333/',// add entry in your computers /etc/hosts mapped to your bot's IP address
+			// web_url			: 'http://localhost:3333/', //web_center url	***Change to this for Rails web_center= http://localhost:3333/  (aka rails s) //  10.0.0.3	beta_bot.local
 			sisbot_url  : 'http://192.168.0.11:3002', //talking to sisbot    //  ... or just put your URL in here '192.168.XX.XXX:3002' << for local Dev Env --insert your ip address + 3000
 			port		: 3001,
 			show_tg		: true
