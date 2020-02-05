30f8d439f1bf35997670fa884a416d08243e6949
CurtisMorice <curtismorice@gmail.com>
2020-02-04 22:37:33 -0500

    fixed

--- js/app.config.js
+++ js/app.config.js
@@ -34,19 +34,19 @@ app.config = {
 		},
 		matt: {
 			base_url		: 'http://app.dev.withease.io:3001/', //local url
-			// api_url		: 'https://webcenter.sisyphus-industries.com/',
-			// web_url		: 'https://webcenter.sisyphus-industries.com/',
-			api_url			: 'http://localhost:3333/', // add entry in your computers /etc/hosts mapped to your bot's IP address
-			web_url			: 'http://localhost:3333/', //web_center url	***Change to this for Rails web_center= http://localhost:3333/  (aka rails s) //  10.0.0.3	beta_bot.local
+			api_url		: 'https://webcenter.sisyphus-industries.com/',
+			web_url		: 'https://webcenter.sisyphus-industries.com/',
+			// api_url			: 'http://localhost:3333/', // add entry in your computers /etc/hosts mapped to your bot's IP address
+			// web_url			: 'http://localhost:3333/', //web_center url	***Change to this for Rails web_center= http://localhost:3333/  (aka rails s) //  10.0.0.3	beta_bot.local
 			sisbot_url  : 'http://192.168.86.33:3002', //talking to sisbot    //  ... or just put your URL in here '192.168.XX.XXX:3002' << for local Dev Env --insert your ip address + 3000
 			port		: 3001,
 		},
 		sisbot: {
 			base_url		: window.location.href,
-			// api_url			: 'http://dev.webcenter.sisyphus-industries.com/', //change these to dev.webcenter for next push to TF and GP
-			// web_url			: 'http://dev.webcenter.sisyphus-industries.com/',			
-			api_url			: 'https://webcenter.sisyphus-industries.com/',
-			web_url			: 'https://webcenter.sisyphus-industries.com/',
+			api_url			: 'http://dev.webcenter.sisyphus-industries.com/', //change these to dev.webcenter for next push to TF and GP
+			web_url			: 'http://dev.webcenter.sisyphus-industries.com/',			
+			// api_url			: 'https://webcenter.sisyphus-industries.com/',
+			// web_url			: 'https://webcenter.sisyphus-industries.com/',
 			sisbot_url  : window.location.href, //talking to sisbot
 			port		: 3001,
 		},
