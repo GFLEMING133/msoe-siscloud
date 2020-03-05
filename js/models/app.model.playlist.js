833e6a8d5b6c349ed885cf55730af8557eac9915
CurtisMorice <curtismorice@gmail.com>
2020-03-05 11:30:45 -0600

    added playlist-rendering for playlist in community, added featured_track to the playlist model.

--- js/models/app.model.playlist.js
+++ js/models/app.model.playlist.js
@@ -37,7 +37,8 @@ app.model.playlist = {
 				next_tracks			: [],				//
 
 				created_by_id		: 'false',
-				created_by_name	: 'User Name'
+				created_by_name	: 'User Name',
+				featured_track  : 0
 			}
 		};
 
