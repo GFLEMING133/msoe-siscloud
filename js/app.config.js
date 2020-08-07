fb751e2d8c64e13217689365c8117ee1c63057f5
CurtisM <curtismorice@gmail.com>
2020-08-07 09:52:30 -0500

    updated configs for 1.10.37

--- js/app.config.js
+++ js/app.config.js
@@ -3,7 +3,7 @@ app.config = {
 	version: '1.9.37', // Read CHANGELOG.md
 	disconnect_timeout_to_stop_polling: 45000, // stop trying to find tables after 45 seconds
 	extended_timeout_to_stop_polling: 90000, // stop trying to find tables when rebooting
-	is_production: false, // cuts out all app.log() output
+	is_production: true, // cuts out all app.log() output
 	is_debug: 'false', // for html to show custom debugging html, use string value
 	is_simulator: false, // are we in a simulator? Mainly for handling bluetooth
 	simulator_ip: '192.168.1.9', // force a specific network address in simulator
